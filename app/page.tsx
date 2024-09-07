f8982a07d6fdcfaac1e125e0c44d8d8de4662a11
TheRoxer <robert.orlowskiradziwill@gmail.com>
2024-09-07 23:09:32 +0200

    fix: deployment content

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
         <div className="flex flex-col justify-start items-center w-1200px pb-8 mt-8">
           <Navbar />
           <Hero />
-          {/* <Services /> */}
-          {/* <Execution /> */}
-          {/* <Testimonials /> */}
-          {/* <Footer /> */}
+          <Services />
+          <Execution />
+          <Testimonials />
+          <Footer />
         </div>
       </main>
     </>
